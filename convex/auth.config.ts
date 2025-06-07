4090286a19694a71bfe2a09afb6e936db145191d
Ravi Kumar <ravibseb96@gmail.com>
2025-06-07 16:59:31 +0530

    Interview Schedule page Created

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://settling-imp-29.clerk.accounts./",
+      domain: "https://settling-imp-29.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ]
--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { api } from "../../../../convex/_generated/api";
 import MeetingModal from "@/components/MeetingModal";
+import LoaderUI from "@/components/LoaderUI";
 
 export default function Home() {
   const router = useRouter();
@@ -31,8 +32,7 @@ export default function Home() {
     }
   };
 
-  if (isLoading) return <p>loading...</p>;
-  // <LoaderUI />
+  if (isLoading) return <LoaderUI />;
 
   return (
     <div className="container max-w-7xl mx-auto p-6">
